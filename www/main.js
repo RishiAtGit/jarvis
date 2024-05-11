a1be6bd4ae6d95f60fd35c5fbb81da477730cbc7
Rishi Raj <rishiraj02102002@gmail.com>
2024-05-11 17:01:34 +0530

    7th

--- www/main.js
+++ www/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
 
     $('.text').textillate({
@@ -44,7 +45,7 @@ $(document).ready(function () {
         eel.playAssistantSound()
         $("#Oval").attr("hidden", true);
         $("#SiriWave").attr("hidden", false);
-        eel.takecommand()()
+        eel.allCommands()()
     });
 
 });
